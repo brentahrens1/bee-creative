b534c8edec57505bdb4263147b278d9bc59479cc
Brent Ahrens <cidida@gmail.com>
2022-01-15 13:44:16 -0800

    fix linting error

--- pages/about-us.js
+++ pages/about-us.js
@@ -21,7 +21,7 @@ export default function AboutUs() {
                 <div className='about-message-content'>
                     <h3>Who We Are</h3>
                     <h1>professional, wacky, creative explorers</h1>
-                    <p>We start by building deep relationships with our clients, allowing us to design better solutions for all of their creative challenges. Let us do the hard work, so you don't have to.</p>
+                    <p>We start by building deep relationships with our clients, allowing us to design better solutions for all of their creative challenges. Let us do the hard work, so you don&apos;t have to.</p>
                 </div>
                 <div className='about-message-image'>
                     <Image width={220} height={280} layout="responsive" priority={true} src='/images/about/watermelon.png' alt="BeeCat Creative | About Us" />
