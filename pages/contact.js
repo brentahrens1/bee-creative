3b3f1637de12be7a57c492ab27193fcc7768d51f
Brent Ahrens <cidida@gmail.com>
2022-01-16 12:56:04 -0800

    make contact page

---
+++ pages/contact.js
@@ -0,0 +1,79 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+
+export default function ContactUs() {
+    const form = useRef();
+    const [ isSuccess, setIsSuccess ] = useState(false)
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+    
+        emailjs.sendForm('service_gykjvxj', 'template_71nweld', form.current, 'user_h2jSyo7kIF8oKQP7E4v9t')
+        .then((result) => {
+            console.log(result.text)
+            setIsSuccess(true)
+        }, (error) => {
+            console.log(error.text)
+        })
+    }
+
+    return (
+        <main className="contact">
+            <Head>
+                <title>BeeCat Creative | Contact Us</title>
+                <meta name="description" content="Beecat Creative Contact Us" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <section className='contact__heading'>
+                <div className='contact-heading-image'>
+                    <Image width={400} height={280} layout="responsive" priority={true} src='/images/contact/phone.png' alt="BeeCat Creative | Contact Us" />
+                </div>
+                <h1>Contact Us</h1>
+            </section>
+            <section className='contact__form'>
+                <h1>Shoot us a Message</h1>
+            <>
+            {
+                !isSuccess ?
+                <form ref={form} onSubmit={sendEmail} className="contact-form">
+                        <label>Name</label>
+                        <div className="name-fields">
+                            <input 
+                            type="text" 
+                            name="first-name" 
+                            placeholder="First Name"
+                            />
+                            <input 
+                            type="text" 
+                            name="last-name" 
+                            placeholder="Last Name" 
+                            />
+                        </div>
+                        <label>Email</label>
+                        <input 
+                        type="email" 
+                        name="email" 
+                        />
+                        <label>Message</label>
+                        <textarea 
+                        type="text"
+                        rows="8" 
+                        cols="50"
+                        name="message"
+                        >
+                        </textarea>
+                        <input type="submit" value="Submit" />
+                </form>
+                :
+                <div className="success">
+                    <h1>Did you know the moon has moonquakes? Weird.</h1>
+                    <h1>Your message is currently traveling through time and space. Destination: our inbox. So be on the lookout for our BeeCat signal… ahem, email</h1>
+                </div>
+            }
+            </>
+            </section>
+        </main>
+    )
+}
