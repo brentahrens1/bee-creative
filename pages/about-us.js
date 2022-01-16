bb12cd3eeb0d5d06238bf40bb72b5a5fd74dc81e
Brent Ahrens <cidida@gmail.com>
2022-01-16 12:57:19 -0800

    fix linting

--- pages/about-us.js
+++ pages/about-us.js
@@ -39,13 +39,13 @@ export default function AboutUs() {
                     </div>
                     <div className='about-col-content'>
                         <h2>Let’s Build Together</h2>
-                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y'all.</p>
+                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y&apos;all.</p>
                     </div>
                 </div>
                 <div className='about-breakdown-col'>
                     <div className='about-col-content'>
                         <h2>Let’s Build Together</h2>
-                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y'all.</p>
+                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y&apos;all.</p>
                     </div>
                     <div className='about-col-image'>
                         <Image width={220} height={280} layout="responsive" priority={true} src='/images/about/robot-2.png' alt="BeeCat Creative | About Us" />
@@ -57,7 +57,7 @@ export default function AboutUs() {
                     </div>
                     <div className='about-col-content'>
                         <h2>Let’s Build Together</h2>
-                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y'all.</p>
+                        <p>It all starts with a conversation. At BeeCat, we take the time to dive deep into your business, focusing on industry trends, competitive landscape, and audience behavior. Then we work with you to lay out a comprehensive plan that positions your brand for success. Dollar, dollar bills y&apos;all.</p>
                     </div>
                 </div>
             </section>
