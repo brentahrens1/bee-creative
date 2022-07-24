38fa4f95f6b23852be1911d7e91308b1e2177ccf
Brent Ahrens <cidida@gmail.com>
2022-07-23 19:56:30 -0700

    add google tag script

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import '../styles/_main.scss'
 import '../components/Layout'
 import Layout from '../components/Layout'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DJX8PDKMN6"
+      />
+      <Script 
+        id="google-tag"
+        strategy="lazyOnload"
+      >
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-DJX8PDKMN6');
+          `
+        }
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
   
 }
